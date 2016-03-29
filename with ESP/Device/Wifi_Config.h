d507a0b547322828723cb2b99d7f26f60fe759d2
BadenMorgan <baden.david.morgan@gmail.com>
2016-03-29 16:19:46 +0200

    working with new display library and new esp code

--- with ESP/Device/Wifi_Config.h
+++ with ESP/Device/Wifi_Config.h
@@ -12,7 +12,7 @@
 //#define SERVER "test.mosquitto.org"  //host for mosquitto broker
 //#define PORT 1883  //port number for mosquitto broker
 
-#define _DEBUG_
+//#define _DEBUG_
 //#define ALLDEBUG // may break functionality but will help diagnose connection issues
 #define AUTORESET //used to autoreset the device if there is no communication with the module or if a complete failuer to connect to wifi occured
 //#define RESETPIN 12
